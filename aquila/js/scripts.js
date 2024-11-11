0de2948d664a847be5da10b52692e554e877d341
tang-edgetech <tang.edgetech@gmail.com>
2024-11-11 12:14:12 +0800

    11 Nov 2024 - v1

--- aquila/js/scripts.js
+++ aquila/js/scripts.js
@@ -1,10 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     $(document).ready(function() {
+        let lastScrollTop = 0;
+    
+        $(window).on('scroll', function() {
+            let currentScroll = $(this).scrollTop();
+    
+            if (currentScroll > lastScrollTop) {
+                $('#masthead .navbar').addClass('scroll-down');
+            } else {
+                $('#masthead .navbar').removeClass('scroll-down');
+            }
+    
+            lastScrollTop = currentScroll;
+        });
+
         if( $('#disclaimerModal')[0] ) {
             var disclaimerModal = new bootstrap.Modal(document.getElementById('disclaimerModal'));
-            setTimeout(function() {
-                disclaimerModal.show();
-            }, 150);
+            // setTimeout(function() {
+            //     disclaimerModal.show();
+            // }, 150);
         }
 
         var paymentOptionsSwiper = new Swiper('#swiper-payment-options', {
@@ -23,10 +37,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
                     spaceBetween: 25,
                     allowTouchMove: false,
                 },
-                768: {
+                1200: {
                     slidesPerView: 8,
                     loop: false,
                 }
+            }
+        });
+
+        $(document).on('click', '.hamburger', function(e) {
+            var $this = $(this);
+            if($this.hasClass('is-active')) {
+                setTimeout(function() {
+                    $this.removeClass('is-active');
+                }, 100);
+            }
+            else {
+                setTimeout(function() {
+                    $this.addClass('is-active');
+                }, 100);
             }
         });
     });
